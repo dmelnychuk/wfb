bf750f99b49e9dfb0d09806e8899011824b42b57
dmelnychuk <5358207@gmail.com>
2025-08-15 16:01:30 +0300

    updating for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   site: "https://example.treelink.com",
   integrations: [tailwind(), sitemap(), icon()],
 });
