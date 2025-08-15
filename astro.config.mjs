c4dac887f50935d475b7e9c3e6863ed9842a4935
dmelnychuk <5358207@gmail.com>
2025-08-15 17:07:05 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,17 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   site: "https://workflowboom.com",
+  base: "/",
   integrations: [tailwind(), sitemap(), icon()],
+  vite: {
+    define: {
+      __IMPORT_META_ENV_SITE__: JSON.stringify("https://workflowboom.com")
+    }
+  }
 });
